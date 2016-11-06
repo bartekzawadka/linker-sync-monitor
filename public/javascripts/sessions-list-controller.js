4e253d35921175a0d106f4eabe26e87a03293b4e
git <kontakt@bartoszzawadka.pl>
2016-11-06 12:00:59 +0100

    Created valid query for all data with logs including sorting

---
+++ public/javascripts/sessions-list-controller.js
@@ -0,0 +1,19 @@
+/**
+ * Created by barte_000 on 2016-11-06.
+ */
+angular.module('LinkerSyncMonitor').controller('SessionsListCtrl', function($scope, $http){
+    $scope.isLoading = true;
+    $scope.sessions = [];
+
+    $http({
+        url: '/api/getSessions',
+        method: 'GET'
+    }).then(function(response){
+        $scope.sessions = response.data.rows;
+        $scope.isLoading = false;
+    }, function(e){
+        console.log(e);
+        $scope.isLoading = false;
+    });
+
+});
--- routes/api.js
+++ routes/api.js
@@ -6,23 +6,43 @@ var router = express.Router();
 var url = require('url');
 var path = require('path');
 var models = require(path.join(__dirname, '..', 'models'));
+var config = require(path.join(__dirname, '..', 'config.json'));
+var sequelize = require('sequelize');
 
 router.get('/getSessions', function(req, res){
 
     var query = {
+        include:{
+            model: models.log,
+            attributes: ['id', 'level', 'type','message',
+                [sequelize.fn('to_char', sequelize.col("logs.\"updatedAt\""), "YYYY-MM-DD HH12:MI:SS"), 'updatedAtFormed']]
+        },
         offset: 0,
-        limit: 20
+        limit: config.defaultDataLimit,
+        order: [
+            ["\"updatedAt\"", "DESC"],
+            [models.log, 'id', 'ASC']
+        ],
+        attributes: [
+            'id',
+            [sequelize.fn('to_char', sequelize.col("session.\"startedAt\""), "YYYY-MM-DD HH12:MI:SS"), 'startedAtFormed'],
+            [sequelize.fn('to_char', sequelize.col("session.\"endedAt\""), "YYYY-MM-DD HH12:MI:SS"), 'endedAtFormed'],
+            "\"updatedAt\""
+        ]
     };
+
     if(req.query.pageSize){
         query.limit = req.query.pageSize;
         if(req.query.pageIndex){
-            query.offset = req.query.pageIndex*req.query.pageSize;
+            query.offset = req.query.pageIndex * req.query.pageSize;
         }
     }
 
     models.session.findAndCountAll(query).then(function(results){
         res.writeHead(200, {"Content-Type": "application/json"});
         res.end(JSON.stringify(results));
+    }).catch(function(e){
+        console.log(e);
     });
 });
 
