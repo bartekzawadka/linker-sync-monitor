29fd6062c828b23813b307863f301b9d6187f84e
git <kontakt@bartoszzawadka.pl>
2016-11-06 21:06:42 +0100

    Session details window tweaked and finished for now.

--- public/javascripts/session-info-controller.js
+++ public/javascripts/session-info-controller.js
@@ -19,5 +19,49 @@ angular.module('LinkerSyncMonitor').controller('SessionInfoCtrl', function($scop
 
     $scope.close = function(){
         $mdDialog.hide();
-    }
+    };
+
+    $scope.getColoForItem = function(item){
+        if(item.level == "ERROR" || item.level == "FATAL")
+            return 'red';
+        if(item.level == "INFO")
+            return 'green';
+        if(item.level == "WARNING")
+            return 'orange';
+        if(item.level == "DEBUG")
+            return 'indigo';
+        return 'blue-grey';
+    };
+
+    $scope.getColorForLevel = function(level){
+        if(!level)
+            return 'blue-grey';
+
+        if(level == "ERROR" || level == "FATAL")
+            return 'red';
+        if(level == "INFO")
+            return 'green';
+        if(level == "WARNING")
+            return 'orange';
+        if(level == "DEBUG")
+            return 'indigo';
+        return 'blue-grey';
+    };
+
+    $scope.getIconForLevel = function(level){
+        if(!level)
+            return null;
+
+        if(level == "ERROR")
+            return 'error';
+        if(level == "FATAL")
+            return 'cancel';
+        if(level == "INFO")
+            return "done";
+        if(level == "WARNING")
+            return 'warning';
+        if(level == "DEBUG")
+            return 'build';
+        return null;
+    };
 });
