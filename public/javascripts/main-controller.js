6ecc715f14e41f78d0cb2a6a022266bc995c293c
git <kontakt@bartoszzawadka.pl>
2016-11-15 00:09:51 +0100

    Added controls for filtering and sorting. Need to realign all now, finish fron-end logic and backend filtering/sorting query support

--- public/javascripts/main-controller.js
+++ public/javascripts/main-controller.js
@@ -3,4 +3,21 @@
  */
 angular.module('LinkerSyncMonitor').controller('MainCtrl', function($scope){
     $scope.selectedMenuItem = 'dashboard';
+    $scope.currentLevel = null;
+    $scope.processedAnyItem = false;
+    $scope.descending = false;
+    $scope.orderDate = "endedAt";
+    $scope.levels = [
+        {name: "INFO", checked: false},
+        {name: "WARNING", checked: false},
+        {name: "DEBUG", checked: false},
+        {name: "ERROR", checked: false},
+        {name: "FATAL", checked: false}
+    ];
+
+    $scope.isFilteringVisible = false;
+
+    $scope.toggleFilteringMenu = function(){
+        $scope.isFilteringVisible = !$scope.isFilteringVisible;
+    }
 });
